cbffb6ff0fcd0fca01d4824c5583d0c0f7b29e3f
YemulaSrikar <104422175+YemulaSrikar@users.noreply.github.com>
2023-02-13 18:53:43 +0530

    Update index.js

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -2,13 +2,17 @@ import {Link} from 'react-router-dom'
 import './index.css'
 
 const NotFound = () => (
-  <Link className="links" to="/not-found">
+  <Link className="links" to="/bad-path">
     <div className="failure-cont">
       <img
         className="not-found-img"
         src="https://assets.ccbp.in/frontend/react-js/ebank-not-found-route-img.png"
         alt="not found"
       />
+      <h1 className="not-found-head">Page Not Found</h1>
+      <p className="not-found-desc">
+        We are sorry, the page you requested could not be found
+      </p>
     </div>
   </Link>
 )
